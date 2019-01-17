15d0d0fbc541593b2f9d57e9fdd0b6729d556b81
slayte <slayteeaton@gmail.com>
2019-01-17 14:05:46 -0600

    rewrote the html for the friends JS file to ease with master merge

--- client/src/pages/friends/Friends.js
+++ client/src/pages/friends/Friends.js
@@ -1,61 +1,68 @@
-import components for the friends list
-import React, { Component } from "react";
-import Jumbotron from "../components/Jumbotron";
-import API from "../utils/API";
+// import components for the friends list
+// import React, { Component } from "react";
+// import Jumbotron from "../components/Jumbotron";
+// import API from "../utils/API";
 
-class FriendsList extends component {}
-state= {
-    friends: [],
-    fullName: "",
-    keychain: "",
-    thumbnail: "", 
-}; 
-componentDidMount(){
-    this.loadFriends();
-}
-loadFriends=()=> {
-    API.getFriends().then(res => this.setState({friends: res.data, fullName: "", keychain: "", thumbnail: ""})
-    ) .catch(err => console.log(err));
-};
-deleteFriendList = id => {
-    API.deleteFriendList(id)
-      .then(res => this.FriendsList())
-      .catch(err => console.log(err));
-  };
+// class FriendsList extends component {}
+// state= {
+//     friends: [],
+//     fullName: "",
+//     keychain: "",
+//     thumbnail: "", 
+// }; 
+// componentDidMount(){
+//     this.loadFriends();
+// }
+// loadFriends=()=> {
+//     API.getFriends().then(res => this.setState({friends: res.data, fullName: "", keychain: "", thumbnail: ""})
+//     ) .catch(err => console.log(err));
+// };
+// deleteFriendList = id => {
+//     API.deleteFriendList(id)
+//       .then(res => this.FriendsList())
+//       .catch(err => console.log(err));
+//   };
 
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
+//   handleInputChange = event => {
+//     const { name, value } = event.target;
+//     this.setState({
+//       [name]: value
+//     });
+//   };
 
 
 render();
 {
     return (
     <Container>
-        <Row>
-            <Col size="">
-                <Thumbnail /> 
-            </Col>
-            <Col size="">
-                <h3>{this.fullName}</h3>
-            </Col>
-            <Col size="">
-            <p>Click the link here to view  the keys on this Friend's Keychain! </p>
-            <a rel="noreferrer noopener" target="_blank" href={keychain}> Friends keychain Link ie. website.com/friend/:id</a>
-            </Col>
-        </Row>
+        <Div class="card">
+            <Image>
+                {/* {this.friendsThumbnail} i.e. the friend's profile image */}
+            </Image>
+            <Div class="card-body">
+                <h5 class="card-title">
+                    {/* {this.fullName} i.e. the friend's first and last name*/}
+                </h5>
+                <p>
+                    Click the button to view 
+                    {/* {this.fullName} */} 
+                    's keys.
+                </p>
+                <a href="" class="btn btn-primary">
+                    {/* {this.friendsKeychainLink} i.e. the link to friends keychain profile.*/}
+                </a>
+            </Div>
+        </Div>
     </Container>
+
     );
 }
 
-//Move to a component item? 
-function Thumbnail({src}){
-    return(
-        <Div className="thumbnail" style= "`url(${src})" />
+// //Move to a component item? 
+// function Thumbnail({src}){
+//     return(
+//         <Div className="thumbnail" style= "`url(${src})" />
         
-    )
-}
+//     )
+// }
 // export function FriendsList();
