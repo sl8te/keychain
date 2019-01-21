6be83da543dc6b6c1b92329f2d5a366b669f2f86
caq204 <mrchrisq@icloud.com>
2019-01-21 13:24:41 -0600

    push boilerplate

---
+++ client/src/pages/search/search.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components";
+import API from "../../utils/API";
+
+class Search extends Component {
+    state = {
+        search : "", 
+        results : []
+    }
+    componentDidMount() {
+
+    }
+    render() {
+        return (
+            <Container fluid>
+            
+             </Container>  
+        )
+    }
+}
